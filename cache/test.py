0329ac7ac176f3758ef0aa28115b786dc0aa019b
wendi <842876912@qq.com>
2018-06-18 01:53:24 +0800

    Crawl from the given seed URL following links matched by link_regex

---
+++ cache/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : test.py
+# @Author: Wade Cheung
+# @Date  : 2018/6/17
+# @Desc  : test
+
+
+import datetime
+import cache.link_crawler
+from cache.DiskCache import DiskCache
+from cache.Downloader import Downloader
+
+download = Downloader()
+myCache = DiskCache()
+
+# download = mytest.download('http://example.webscraping.com', {'User_agent': 'wswp'})
+# myCache.url_to_path('http://example.webscraping.com')
+
+starttime = datetime.datetime.now()
+# cache.link_crawler.link_crawler(seed_url='http://example.webscraping.com/', link_regex='/(index|view)',
+#                                 cache=DiskCache())
+cache.link_crawler.link_crawler('http://example.webscraping.com/', '/(index|view)', max_depth=3, cache=DiskCache())
+endtime = datetime.datetime.now()
+print('时长为' + str((endtime - starttime)))
+
+
